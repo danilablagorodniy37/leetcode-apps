9500fe2e8a776612561139dbbba700ba0835ae1b
blago1 <74470460+danilablagorodniy37@users.noreply.github.com>
2026-02-07 20:20:04 +0000

    Rename variable 'res' to 'result' for clarity

--- solutions/771/01.py
+++ solutions/771/01.py
@@ -1,8 +1,8 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
 
-        res = 0               # Counter for how many jewels we found
-        for s in stones:      # Loop through each character (stone) in stones
-            if s in jewels:   # Check if this stone type is listed in jewels
-                res += 1      # If yes, increase the counter
-        return res            # Return the total count
+        result = 0               # Counter for how many jewels we found
+        for s in stones:         # Loop through each character (stone) in stones
+            if s in jewels:      # Check if this stone type is listed in jewels
+                result += 1      # If yes, increase the counter
+        return result            # Return the total count
